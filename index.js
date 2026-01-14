3a7a24ec0d0ee1193d23d36f6f112f72aea56e64
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-14 15:57:05 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1 . Hello World
+console.log("Nodejs Backend Started");
+
+// 2. Varialbles
+let name = "Aman";     //string variable
+let age = 22;          //number variable
+const country="india"; //constant variable
+let isstudent = true;  //boolean variable
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isstudent);
+
+// 3. Data Types
+let score;              //undefined variable
+let data = null;        //null variable
+
+console.log(score);
+console.log(data);
+
+// Object
+let user = {
+    name : "Aman",
+}
