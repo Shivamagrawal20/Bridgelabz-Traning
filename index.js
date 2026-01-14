06385a185bb9aa2d453e19bde926f5a738d9fe8b
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-14 16:51:30 +0530

    String Methods

--- index.js
+++ index.js
@@ -22,4 +22,47 @@ console.log(data);
 // Object
 let user = {
     name : "Aman",
+    age : 22,
+    email : "rahul@gmail.com",
+};
+console.log(user);
+console.log(user.name);
+
+// Aray
+let fruits = ["apple", "banana", "mango", "grapes"];
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+console.log(fruits);
+console.log(fruits[2]);
+
+// 4. Operators
+let a = 10;
+let b = 5;
+// Arithmetic Operators
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+// condition (if else)
+let loginAge = 18;
+if (loginAge >= 18) {
+    console.log("User can login");
+} else {
+    console.log("User cannot login");
 }
+
+// loop (for loop)
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteration:", i);
+}
+
+
+// Simple Backend Login
+const admin = {
+    username: "admin",
+    password: "admin123"
+}
+
+
+
---
+++ index2.js
@@ -0,0 +1,28 @@
+let s1="hello world";
+let s2='hello world';
+let s3="node.js is fun";
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+//String Methods
+let text = "JavaScript";
+console.log("Length:", text.length);
+
+//Access Characters (Index)
+let word = "Node";
+console.log("First Character:", word[0]);        // N
+console.log("Third Character:", word.charAt(2)); // d
+
+//toUpperCase() and toLowerCase()    
+console.log("Uppercase:", text.toUpperCase());
+console.log("Lowercase:", text.toLowerCase());  
+let city = "new york";
+
+//trim()
+let message = "   Hello World!   ";
+console.log("Trimmed Message:", message.trim());
+
+//include()
+let sentence = "I love programming in JavaScript";
