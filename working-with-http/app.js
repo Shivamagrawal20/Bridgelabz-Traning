80ec1537e07ce6f6c4e24db65105d74c411a7f03
Shivam Agrawal <shivamprfs@gmail.com>
2026-02-02 14:10:00 +0530

    [Shivam].add file handeling

--- working-with-http/app.js
+++ working-with-http/app.js
@@ -4,12 +4,13 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case "/":
             console.log(req.method);
+            console.log(req);
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(`
             <h2>Contact</h2>
             <h1>Name - Shivam Agrawal</h1>
             <h1>Email - shivam@xyz</h1>
-        `);
+`);
             break;
 
         default:
